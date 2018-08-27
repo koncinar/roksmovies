c41591bf15ab1ea7c725f64b892fe340ca2bb7f1
Rok <rok.koncina@aerlingus.com>
2018-08-27 20:53:18 +0100

    Loading images for each movie from the URL specified in the API response Glide library added for asynchronous loading of the images from the internet

--- app/src/main/java/rocks/koncina/roksmovies/BindingAdapters.kt
+++ app/src/main/java/rocks/koncina/roksmovies/BindingAdapters.kt
@@ -4,8 +4,14 @@ import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
 import android.databinding.BindingAdapter
+import android.graphics.drawable.Drawable
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.DrawableImageViewTarget
+import com.bumptech.glide.request.transition.Transition
 import rocks.koncina.roksmovies.movieslist.api.Movie
 import rocks.koncina.roksmovies.movieslist.view.MoviesListAdapter
 
@@ -20,5 +26,28 @@ fun RecyclerView.setMovies(moviesLiveData: LiveData<List<Movie>>) {
     moviesLiveData.observe(context as LifecycleOwner,
             Observer { moviesListAdapter.movies = it.orEmpty() })
 
+}
+
+@BindingAdapter("thumbnail")
+fun ImageView.setThumbnail(url: String?) {
+    if (url == null) {
+        setImageDrawable(null)
+        visibility = View.GONE
+
+    } else {
+        Glide.with(context)
+                .load(BuildConfig.URL_THE_MOVIE_DB_THUMBNAILS + url)
+                .into(object : DrawableImageViewTarget(this) {
+                    override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                        visibility = View.VISIBLE
+                        super.onResourceReady(resource, transition)
+                    }
+
+                    override fun onLoadFailed(errorDrawable: Drawable?) {
+                        visibility = View.GONE
+                        super.onLoadFailed(errorDrawable)
+                    }
+                })
+    }
 }
 
