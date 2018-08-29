662ba5bbf35e941807e00a1d868632e8803e1fbe
Rok <rok.koncina@aerlingus.com>
2018-08-29 15:45:50 +0100

    Improvements to the search UI - the native search component is used in the ActionBar - the FAB, the bottom search bar and the logic were removed

--- app/src/main/java/rocks/koncina/roksmovies/MainActivity.kt
+++ app/src/main/java/rocks/koncina/roksmovies/MainActivity.kt
@@ -1,37 +1,38 @@
 package rocks.koncina.roksmovies
 
-import android.arch.lifecycle.ViewModelProviders
-import android.databinding.DataBindingUtil
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
+import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
-import rocks.koncina.roksmovies.databinding.ActivityMainBinding
 import rocks.koncina.roksmovies.factories.InstanceFactory
 import rocks.koncina.roksmovies.factories.ViewModelFactory
 import rocks.koncina.roksmovies.moviedetails.view.MovieDetailsFragment
 import rocks.koncina.roksmovies.movieslist.api.Movie
 import rocks.koncina.roksmovies.movieslist.view.MoviesListFragment
 
+
 class MainActivity : AppCompatActivity() {
 
     val viewModelFactory by lazy { ViewModelFactory(InstanceFactory(applicationContext)) }
 
+    internal var searchMenuItem: MenuItem? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        binding.viewModel = ViewModelProviders.of(this, viewModelFactory)
-                .get(MainViewModel::class.java)
-                .also {
-                    it.onStartSearch = this::startSearch
-                }
 
         supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment, MoviesListFragment.newInstance())
                 .commit()
+
+        handleIntent(intent)
     }
 
     fun openMovieDetails(movie: Movie) {
@@ -46,6 +47,39 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.fragment, MoviesListFragment.newInstance(query))
                 .addToBackStack(null)
                 .commit()
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            startSearch(query)
+
+            //close the searchView
+            searchMenuItem?.apply {
+                val searchView = actionView as SearchView
+                searchView.clearFocus()
+                searchView.isIconified = true
+                collapseActionView()
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchMenuItem = menu.findItem(R.id.action_search)
+        (searchMenuItem?.actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -65,4 +99,8 @@ fun Fragment.setTitle(title: String) {
 
 fun Fragment.showUp(show: Boolean) {
     (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(show)
+}
+
+fun Fragment.showSearch(show: Boolean) {
+    (activity as MainActivity).searchMenuItem?.isVisible = show
 }
--- app/src/main/java/rocks/koncina/roksmovies/factories/ViewModelFactory.kt
+++ app/src/main/java/rocks/koncina/roksmovies/factories/ViewModelFactory.kt
@@ -2,7 +2,6 @@ package rocks.koncina.roksmovies.factories
 
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
-import rocks.koncina.roksmovies.MainViewModel
 import rocks.koncina.roksmovies.moviedetails.viewmodel.MovieDetailsViewModel
 import rocks.koncina.roksmovies.movieslist.viewmodel.MoviesListViewModel
 import java.security.InvalidParameterException
@@ -17,10 +16,6 @@ class ViewModelFactory(private val instanceFactory: InstanceFactory) : ViewModel
 
         if (modelClass.isAssignableFrom(MovieDetailsViewModel::class.java)) {
             return MovieDetailsViewModel(instanceFactory.movieDetailsRepository) as T
-        }
-
-        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
-            return MainViewModel() as T
         }
 
         throw InvalidParameterException("Unknown model class $modelClass")
